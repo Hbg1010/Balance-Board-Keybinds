c5a295d4f94f03bd4c633b1d39a72b12a58838ab
hbg1010 <harrisong26@parkschool.net>
2025-03-03 11:59:18 -0500

    cleaned this up

--- src/customGUI/WeightPopup.hpp
+++ src/customGUI/WeightPopup.hpp
@@ -1,9 +1,11 @@
+#pragma once
+
 #include <Geode/Geode.hpp>
-#include "../BalanceBoard.hpp"
+
 using namespace geode::prelude;
 
 
-class WeightPopup final : public Popup<float const&> {
+class WeightPopup final : public geode::Popup<float const&> {
 public:
     static WeightPopup* create(float const& input);
 protected:
