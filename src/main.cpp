c2a27402adadf326a9ae22ace9ba81025cdfdc18
hbg1010 <harrisong26@parkschool.net>
2025-03-03 13:05:07 -0500

    cleaned up main, added keybinds

--- src/main.cpp
+++ src/main.cpp
@@ -9,60 +9,6 @@ using namespace geode::prelude;
 
 #include <Geode/modify/PlayLayer.hpp>
 
-// class $modify(PlayLayer) {
-
-//     struct Fields {
-//         bool pauseThis;
-//         wiimote* mote;
-//     };
-
-    
-//     bool init(GJGameLevel* level, bool useReplay, bool p2) {
-//         if (!PlayLayer::init(level, useReplay, p2)) return false;
-//         wiimote** remote;
-//         wiimote* balanceBoard = nullptr;
-//         for (unsigned int i = 0; i < 5; i++) {
-//             wiimote *next = new wiimote;
-//             if (next->Connect(wiimote::FIRST_AVAILABLE)) {
-//                 if (!next->IsBalanceBoard()) {
-//                     balanceBoard = next;
-//                     break;
-//                 } else {
-//                     remote[i] = next;
-//                 }
-//             }
-//         }
-
-//         if (std::ref(balanceBoard) != nullptr) {
-//             log::debug("x");
-//             m_fields->mote = balanceBoard;
-//         } else {
-            
-//         }
-//         // auto alert = FLAlertLayer::create(
-//         //     "Title",
-//         //     "Hi mom!",
-//         //     "OK"
-//         // );
-//         // alert->m_scene = this;
-//         // alert->show();
-//         // m_fields->pauseThis = true;
-//         return true;
-//     }
-
-//     void resetLevel() {
-//         if (m_fields->pauseThis) pauseGame(true);
-//         if (m_fields->mote->Button.A()) {
-//             log::debug("A");
-//         }
-//         PlayLayer::resetLevel();
-//     }
-// };
-
-// $on_mod(Loaded) {
-//     // BindManager::get()->attachDevice("balance_board"_spr, &BBKeybind::parse);
-// }
-
 class BalanceBoardChecker : public CCObject{
 protected:
     bool m_state = false;
@@ -114,8 +60,8 @@ $execute {
         "init"_spr,
         "Balance Board Init",
         "waits for the player to step on the balance board before starting the round!",
-        // Default binds
-        { BBKeybind::create(false), Keybind::create(KEY_Q, Modifier::None) },
+        // Default binds 
+        { BBKeybind::create(false), Keybind::create(KEY_Q, Modifier::None) }, // TODO: REMOVE Q
         "BalanceBoard"
     });
 
